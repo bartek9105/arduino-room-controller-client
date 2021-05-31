9f181f26d0ac6ec99300ed783cd7ffc0cb258e67
bartek9105 <bartek9105@gmail.com>
2021-05-31 12:33:08 +0200

    Create more info button and use router link for single room view

--- src/components/RoomTilesInfo.vue
+++ src/components/RoomTilesInfo.vue
@@ -90,6 +90,27 @@
           <span class="text-gray-500 text-sm">38%</span>
         </div>
       </div>
+      <div
+        class="flex flex-col items-center text-white bg-blue-500 shadow-md p-4 cursor-pointer hover:bg-blue-400 transition-all ease-in-out"
+      >
+        <svg
+          class="w-6 h-6 mb-2"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M14 5l7 7m0 0l-7 7m7-7H3"
+          ></path>
+        </svg>
+        <router-link to="/room/1">
+          <span>More info</span>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
